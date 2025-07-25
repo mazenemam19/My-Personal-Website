38a6c51b9f518e5b909be40bb5d7dac1b929e212
Mazen <mazenemam19@proton.me>
2025-07-26 01:29:33 +0300

    add SpeedInsights

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import {
   AboutSection,
   ArticlesSection,
@@ -8,21 +8,22 @@ import {
   ProjectsSection,
   Seo,
 } from "gatsby-theme-portfolio-minimal";
-import Banner from '../components/banners/ceasefire'
-
-import "./index.css"
+import React from "react";
+import Banner from "../components/banners/ceasefire";
+import "./index.css";
 
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Mazen Emam" />
-      <Banner/>
+      <Seo title='Mazen Emam' />
+      <Banner />
+      <SpeedInsights />
       <Page useSplashScreenAnimation>
-        <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
-        <AboutSection sectionId="about" heading="About Me" />
-        <InterestsSection sectionId="skills" heading="Skills" />
-        <ProjectsSection sectionId="projects" heading="My Side Projects" />
+        <HeroSection sectionId='hero' />
+        <ArticlesSection sectionId='articles' heading='Latest Articles' sources={["Medium"]} />
+        <AboutSection sectionId='about' heading='About Me' />
+        <InterestsSection sectionId='skills' heading='Skills' />
+        <ProjectsSection sectionId='projects' heading='My Side Projects' />
       </Page>
     </>
   );
